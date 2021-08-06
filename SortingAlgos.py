8807070ac44ecefe78576b98c0f5e619ca290e70
Macdeini <84424393+Macdeini@users.noreply.github.com>
2021-08-05 22:28:29 -0400

    Update SortingAlgos.py
    Cleaned up the code

--- SortingAlgos.py
+++ SortingAlgos.py
@@ -2,25 +2,45 @@ import pygame
 import random
 import sys
 SCREEN_WIDTH, SCREEN_HEIGHT = 1880, 1000
-DELAY = 1
 
 
-def force_quit():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+class Button:
+    """Button class"""
+
+    def __init__(self, left, top, width, height, text_content, algo=None):
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+        self.text = text_content
+        self.text_offset = 10
+        self.text_size = 12
+        self.font = "Comic Sans MS"
+        self.bg = "white"
+        self.text_color = "black"
+        self.algo = algo
+
+    def draw(self, screen):
+        button = pygame.Rect(self.left, self.top, self.width, self.height)
+        pygame.draw.rect(screen, self.bg, button)
+        comic_sans = pygame.font.SysFont(self.font, self.text_size)
+        button_text = comic_sans.render(self.text, False, self.text_color)
+        screen.blit(button_text, (self.left+self.text_offset, self.top+self.text_offset))
 
 
 def check_quit():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return True
-    return False
+            sys.exit()
 
 
-def draw_list(screen, lst, curr=None):
-    force_quit()
+def draw_list(screen, lst, curr=None, buttons=None):
+    """Draws the current state of the list and buttons if needed. Highlights the element at curr if wanted"""
+    check_quit()
     screen.fill("black")
+    if buttons:
+        for button in buttons:
+            button.draw(screen)
     starting_pos = 100
     bar_width = 10
     colors = ["white", "yellow", "blue", "red", "green", "purple", "orange", "black"]
@@ -34,104 +54,94 @@ def draw_list(screen, lst, curr=None):
     pygame.display.update()
 
 
-def visual_bubble_sort(screen, lst):
-    size = len(lst)
-    for i in range(size):
-        for j in range(size-i-1):
-            if lst[j] >= lst[j+1]:
-                lst[j], lst[j+1] = lst[j+1], lst[j]
-            draw_list(screen, lst, j+1)
-            pygame.time.wait(DELAY)
-            pygame.event.pump()
-
-
-def visual_insertion_sort(screen, lst):
-    for i in range(1, len(lst)):
-        key = lst[i]
-        j = i-1
-        draw_list(screen, lst, i)
-        pygame.time.wait(DELAY)
-        while j >= 0 and key < lst[j]:
-            lst[j + 1] = lst[j]
-            j -= 1
-        lst[j + 1] = key
-        draw_list(screen, lst, j+1)
-        pygame.time.wait(DELAY)
-        pygame.event.pump()
-
-
-def visual_selection_sort(screen, lst):
-    for i in range(len(lst)):
-        min_idx = i
-        draw_list(screen, lst, i)
-        pygame.time.wait(DELAY)
-        for j in range(i+1, len(lst)):
-            if lst[min_idx] > lst[j]:
-                min_idx = j
-        lst[i], lst[min_idx] = lst[min_idx], lst[i]
-        draw_list(screen, lst, min_idx)
-        pygame.time.wait(DELAY)
-        pygame.event.pump()
-
-
-def get_menu_option():
-    pos = (0, 0)
+def get_menu_option(buttons):
+    """Checks for the position of a mouse click and returns the button if clicked"""
+    pos = (-1, -1)
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
         elif event.type == pygame.QUIT:
             sys.exit()
-    if 100 <= pos[0] <= 100+100 and 925 <= pos[1] <= 925+50:
-        return "randomize"
-    elif 100+155 <= pos[0] <= 100+155+100 and 925 <= pos[1] <= 925+50:
-        return visual_bubble_sort
-    elif 100+155*2 <= pos[0] <= 100+155*2+100 and 925 <= pos[1] <= 925+50:
-        return visual_selection_sort
-    elif 100+155*3 <= pos[0] <= 100+155*3+100 and 925 <= pos[1] <= 925+50:
-        return visual_insertion_sort
-    else:
-        return None
-
-
-def draw_buttons(screen):
-    randomize_button = pygame.Rect(100, 925, 100, 50)
-    bubble_button = pygame.Rect(100+155, 925, 100, 50)
-    selection_button = pygame.Rect(100+155*2, 925, 100, 50)
-    insertion_button = pygame.Rect(100+155*3, 925, 100, 50)
-    pygame.draw.rect(screen, "white", randomize_button, 0, 10)
-    pygame.draw.rect(screen, "white", bubble_button, 0, 10)
-    pygame.draw.rect(screen, "white", selection_button, 0, 10)
-    pygame.draw.rect(screen, "white", insertion_button, 0, 10)
-    comic_sans = pygame.font.SysFont('Comic Sans MS', 12)
-    randomize_text = comic_sans.render("Randomize", False, "black")
-    bubble_text = comic_sans.render("Bubble Sort", False, "black")
-    selection_text = comic_sans.render("Selection Sort", False, "black")
-    insertion_text = comic_sans.render("Insertion Sort", False, "black")
-    screen.blit(randomize_text, (100+10, 925+10))
-    screen.blit(bubble_text, (100+155+10, 925+10))
-    screen.blit(selection_text, (100+155*2+10, 925+10))
-    screen.blit(insertion_text, (100+155*3+10, 925+10))
-    pygame.display.update()
+    for button in buttons:
+        if button.left <= pos[0] <= button.left+button.width and button.top <= pos[1] <= button.top+button.height:
+            return button
+    return None
 
 
 def main():
+    """Sets parameters and handles the main program loop"""
+    MIN, MAX, NUM_ITEMS = 1, 90, 150
+    lst = [random.randint(MIN, MAX) for _ in range(NUM_ITEMS)]
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.font.init()
     pygame.display.set_caption("Sort Visuals")
-    lst = [random.randint(1, 90) for _ in range(150)]
     draw_list(screen, lst)
-    draw_buttons(screen)
-    running = False
-    while not running:
-        option = get_menu_option()
-        if option == "randomize":
-            lst = [random.randint(1, 90) for _ in range(150)]
-            draw_list(screen, lst)
-        elif option is not None:
-            option(screen, lst)
-        draw_buttons(screen)
+
+    randomize_button = Button(100, 925, 100, 50, "Randomize")
+    bubble_button = Button(255, 925, 100, 50, "Bubble Sort", visual_bubble_sort)
+    selection_button = Button(410, 925, 100, 50, "Selection Sort", visual_selection_sort)
+    insertion_button = Button(565, 925, 100, 50, "Insertion Sort", visual_insertion_sort)
+    # stop_button = Button(720, 925, 100, 50, "Stop")
+    buttons = [randomize_button, bubble_button, selection_button, insertion_button]
+
+    running = True
+    while running:
+        draw_list(screen, lst, None, buttons)
+        pressed = get_menu_option(buttons)
+        if pressed is not None:
+            if pressed.algo is not None:
+                pressed.algo(screen, lst)
+            elif pressed.text == randomize_button.text:
+                random.shuffle(lst)
+                draw_list(screen, lst)
     pygame.quit()
+
+
+def buffer(screen, lst, delay, curr=None):
+    """Draws the list during sorting and handles timing"""
+    draw_list(screen, lst, curr)
+    pygame.time.wait(delay)
+    pygame.event.pump()
+
+
+def visual_bubble_sort(screen, lst):
+    """Bubble sort with modifications for visuals"""
+    DELAY = 2
+    size = len(lst)
+    for i in range(size):
+        for j in range(size-i-1):
+            if lst[j] >= lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+            buffer(screen, lst, DELAY, j+1)
+
+
+def visual_selection_sort(screen, lst):
+    """Selection sort with modifications for visuals"""
+    DELAY = 25
+    for i in range(len(lst)):
+        min_idx = i
+        buffer(screen, lst, DELAY, i)
+        for j in range(i+1, len(lst)):
+            if lst[min_idx] > lst[j]:
+                min_idx = j
+        lst[i], lst[min_idx] = lst[min_idx], lst[i]
+        buffer(screen, lst, DELAY, min_idx)
+
+
+def visual_insertion_sort(screen, lst):
+    """Insertion sort with modifications for visuals"""
+    DELAY = 25
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i-1
+        buffer(screen, lst, DELAY, i)
+        while j >= 0 and key < lst[j]:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+        buffer(screen, lst, DELAY, j+1)
 
 
 if __name__ == '__main__':
